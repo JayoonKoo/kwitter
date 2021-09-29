2e9d7f58c15923202f9bfc0dcf5df528bccbbd77
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-29 15:56:55 +0900

    feat: 로그아웃 버튼 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,19 @@
 import React from 'react'
+import {authService} from 'fbase';
+import { useHistory } from 'react-router';
 
-const Profile = () => <span>Profile</span>;
+const Profile = ({auth}) => {
+	const history = useHistory()
+	const logOut = async () => {
+		await authService.signOut(auth);
+		history.push("/");
+	}
+	
+	return (
+		<>
+			<button onClick={logOut}>로그아웃</button>
+		</>
+	);
+}
+
 export default Profile;
