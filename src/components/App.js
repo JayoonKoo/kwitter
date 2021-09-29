749df31abd09539ad5927d5c0e1a3e0a44f5f698
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-29 17:26:49 +0900

    feat: firestore props 로 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,32 @@
 import React, { useEffect, useState } from "react";
-import {authService} from "fbase";
+import { authService, dbService } from "fbase";
 import AppRouter from "components/Router";
 
 function App() {
-	const auth = authService.getAuth();
-	const [isLoading, setIsLoading] = useState(true);
+  const auth = authService.getAuth();
+  const firestore = dbService.getFirestore();
+  const [isLoading, setIsLoading] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	useEffect(() => {
-		authService.onAuthStateChanged(auth, (user) => {
-			if (user) {
-				setIsLoggedIn(true);
-			} else {
-				setIsLoggedIn(false);
-			}
-			setIsLoading(false);
-		})
-	}, [])
+  useEffect(() => {
+    authService.onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setIsLoading(false);
+    });
+  }, []);
   return (
-		<>
-		{isLoading? "Loading..." : <AppRouter isLoggedIn={isLoggedIn} auth={auth}/>}
-		</>
-	);
+    <>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <AppRouter isLoggedIn={isLoggedIn} auth={auth} firestore={firestore} />
+      )}
+    </>
+  );
 }
 
 export default App;
