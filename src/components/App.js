e10d1416091b8b0574816d840130c2edd88b526c
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-29 15:54:56 +0900

    fix: 로그인 상태관리 변경
     useEffect 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {authService} from "fbase";
 import AppRouter from "components/Router";
 
 function App() {
-	const user = authService.currentUser;
-  const [isLoggedIn, setIsLoggedIn] = useState(user);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+	const auth = authService.getAuth();
+	const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	useEffect(() => {
+		authService.onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
+			}
+			setIsLoading(false);
+		})
+	}, [])
+  return (
+		<>
+		{isLoading? "Loading..." : <AppRouter isLoggedIn={isLoggedIn} auth={auth}/>}
+		</>
+	);
 }
 
 export default App;
