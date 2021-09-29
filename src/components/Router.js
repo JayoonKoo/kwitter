ada1180301d03c220ccfee0d99e46bd61c7028a3
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-29 15:55:53 +0900

    feat: 라우터 추가
    profile 추가
    
    auth props로 전달하게 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from 'components/Navigation';
+import Profile from "routes/Profile";
 
-const AppRouter = ({ isLoggedIn }) => {
+const AppRouter = ({ isLoggedIn , auth }) => {
   return (
     <Router>
+			{isLoggedIn ? <Navigation /> : null}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile auth={auth}/>
+            </Route>
           </>
         ) : (
           <Route exact path="/">
-            <Auth />
+            <Auth auth={auth}/>
           </Route>
         )}
       </Switch>
