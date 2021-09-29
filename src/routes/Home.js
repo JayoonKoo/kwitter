a8acb5522757d4da7bc062df0acf69b5a24b1daf
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-30 01:09:51 +0900

    refactor: 트윗 read 로직 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,40 +1,46 @@
 import React, { useEffect, useState } from "react";
-import {dbService} from 'fbase';
+import { dbService } from "fbase";
+import Kweet from "components/Kweet";
 
-const Home = ({firestore, userObj}) => {
-	const [content, setContent] = useState("");
-	const [kweets, setKweets] = useState([]);
+const Home = ({ firestore, userObj }) => {
+  const [content, setContent] = useState("");
+  const [kweets, setKweets] = useState([]);
 
   const onChange = (event) => {
-		const {target: {value}} = event;
-		setContent(value);
-	};
+    const {
+      target: { value },
+    } = event;
+    setContent(value);
+  };
 
-	useEffect(() => {
-		dbService.onSnapshot(dbService.collection(firestore, "kweet"), (snapshot) => {
-			const kweetObj = snapshot.docs.map((doc) => ({
-				...doc.data(),
-				id: doc.id
-			}))
-			setKweets(kweetObj);
-		});
-	}, []);
+  useEffect(() => {
+    dbService.onSnapshot(
+      dbService.collection(firestore, "kweet"),
+      (snapshot) => {
+        const kweetObj = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setKweets(kweetObj);
+      }
+    );
+  }, []);
 
-	const onSubmit = async (event) => {
-		event.preventDefault();
-		await dbService.addDoc(dbService.collection(firestore, "kweet"), {
-			content,
-			createdAt: dbService.serverTimestamp(),
-			createrId: userObj.uid,
-		})
-		setContent("");
-	} 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.addDoc(dbService.collection(firestore, "kweet"), {
+      content,
+      createdAt: dbService.serverTimestamp(),
+      createrId: userObj.uid,
+    });
+    setContent("");
+  };
 
   return (
     <>
       <form onSubmit={onSubmit}>
         <input
-					value={content}
+          value={content}
           type="text"
           onChange={onChange}
           placeholder="트윗을 적어 주세요."
@@ -43,13 +49,16 @@ const Home = ({firestore, userObj}) => {
         <input type="submit" value="tweet" />
       </form>
 
-			<ul>
-				{kweets.map((kweet) => (
-					<li key={kweet.id}>
-						<h4>{kweet.content}</h4>
-					</li>
-				))}
-			</ul>
+      <ul>
+        {kweets.map((kweet) => (
+          <Kweet
+            key={kweet.id}
+            kweet={kweet}
+            isOwner={kweet.createrId === userObj.uid}
+						firestore={firestore}
+          />
+        ))}
+      </ul>
     </>
   );
 };
