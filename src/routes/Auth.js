1e0f1af7392d416b1b60665c73fc61288b1d399c
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-29 15:56:29 +0900

    feat: 로그인 기능 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import {authService} from 'fbase'
 
-const Auth = () => {
+const Auth = ({auth}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+	const [newAccount, setNewAccount] = useState(true);
+	const [error, setError] = useState("");
 
   function onChange(e) {
 		const {target: {name, value}} = e;
@@ -14,9 +17,37 @@ const Auth = () => {
 		}
 	}
 
-  function onSubmit(e) {
+  async function onSubmit(e) {
     e.preventDefault();
+		try {
+			if (newAccount) {
+				await authService.createUserWithEmailAndPassword(auth, email, password);
+			} else {
+				await authService.signInWithEmailAndPassword(auth, email, password);
+			}
+		} catch (error) {
+			setError(error.message);
+		}
   }
+
+	function toggleSignIn() {
+		setNewAccount(prev=> !prev);
+	}
+
+	const socialLogin = async (event) => {
+		const {target: {name}} = event;
+		let provider;
+		if (name === "google") {
+			provider = new authService.GoogleAuthProvider();
+		} else if (name === "github"){
+			provider = new authService.GithubAuthProvider();
+		}
+		try {
+			await authService.signInWithPopup(auth, provider);
+		} catch (error) {
+			console.log(`error`, error);
+		}
+	}
 
   return (
     <div>
@@ -37,11 +68,13 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount? "회원가입": "로그인"} />
+				<span onClick={toggleSignIn}>{!newAccount? "회원가입": "로그인"}</span>
+				{error}
       </form>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={socialLogin} name="google" >Continue with Google</button>
+        <button onClick={socialLogin} name="github" >Continue with Github</button>
       </div>
     </div>
   );
